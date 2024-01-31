4cf368b5853b2ef9249d6d11748d77eecff5a6e9
지도서비스플랫폼팀/이재석 <jsle_e@hyundai-autoever.com>
2024-01-31 16:56:45 +0900

    test_1

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "RoutoMaps",
-            targets: ["RoutoMaps","RoutoMapSDK"]),
+            targets: ["RoutoMaps","RoutoMapSDK","MapboxCommon","MapboxCoreMaps","MapboxMaps","Turf"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
